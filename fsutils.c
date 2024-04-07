32fd9da5b4374d8e53bc2653e60e4cb1accde4c7
adrianjorge.sanchez <adrianjorge.sanchez@students.salle.url.edu>
2024-04-07 18:29:12 +0200

    code more clean

--- fsutils.c
+++ fsutils.c
@@ -2,11 +2,13 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc == 3 && strcmp(argv[1], "--info") == 0) {
-        char* file_system = (argv[2]);
-        printf("File system: %s\n", file_system);
-    } else {
+
+    if (argc != 3) {
         printf("Usage: %s --info <file system>\n", argv[0]);
-    }
+    } 
+    
+    char* file_system = (argv[2]);
+    printf("File system: %s\n", file_system);
+    
     return 0;
 }
